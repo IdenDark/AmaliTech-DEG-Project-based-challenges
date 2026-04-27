cdc1b73cedd108619794a7ede3e9c455aeabc0fe
IdenDark <bonheurndacyayisenga@gmail.cm>
2026-04-27 12:58:00 +0200

    Project Initiation at "0%" Progress

---
+++ backend/Pulse-Check/pulse/src/test/java/com/pulsecheck/pulse/PulseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pulsecheck.pulse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PulseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
